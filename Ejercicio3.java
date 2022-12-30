0528b76f733b84ca11c2b22f3809a1143d548098
PierinnaRM <47922071+PierinnaRM@users.noreply.github.com>
2022-12-29 20:11:49 -0500

    Add files via upload
    Ejercicio 3 - Curso de Java Básico

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+package com.ejercicio;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        String[] palabras = {"Hola", "a", "todos", "mis", "amigos"};
+
+        for(String p : palabras) {
+            System.out.print(p);
+            System.out.print(" ");
+        }
+    }
+}
